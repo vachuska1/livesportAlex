f4e583a345a220749b7f3f397240ec1310431fa9
Alexander Vachuška <a.vachuska@email.cz>
2020-11-12 15:36:02 +0100

    Update Container.tsx

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -4,6 +4,7 @@ import { Content } from "./Content/Content";
 
 export const Container: React.FC = () => {
   return (
+    // tady je zbytecnej ten element <> a </> to tam vubec byt nemusi... vis proc?
     <>
       <div className="Container">
         <Content>sdsadsadasdsds</Content>
