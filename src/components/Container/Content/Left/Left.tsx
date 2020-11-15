db87ff8304a5187245fd7cb6acd541f93b2fb280
Ales Vachuska <aless.vachuska@seznam.cz>
2020-11-15 11:57:02 +0100

    aleslivesport

--- src/components/Container/Content/Left/Left.tsx
+++ src/components/Container/Content/Left/Left.tsx
@@ -7,21 +7,17 @@ import { More } from "./More/More";
 
 export const Left: React.FC = () => {
   return (
-    <>
-      {" "}
-      // zbytecny
-      <div className="Left">
-        <div className="grid">
-          <Country />
-          <States />
-          {/* <States> Albánie </States>
-          <States>Alžírsko </States>
-          <States>Andorra</States>
-          <States>Anglie</States>
-          <States>ssss</States> */}
-          <More />
-        </div>
+    <div className="Left">
+      <div className="grid">
+        <Country />
+        <States />
+        {/* <States> Albánie </States>
+        <States>Alžírsko </States>
+        <States>Andorra</States>
+        <States>Anglie</States>
+        <States>ssss</States> */}
+        <More />
       </div>
-    </>
+    </div>
   );
 };
