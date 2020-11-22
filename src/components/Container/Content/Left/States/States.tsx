573c044d68d5c6f064bf1940ade0293d7f5dc140
alexander.vachuska <alexander.vachuska@livesport.eu>
2020-11-22 21:13:24 +0100

    predani komponent do hlavni

--- src/components/Container/Content/Left/States/States.tsx
+++ src/components/Container/Content/Left/States/States.tsx
@@ -1,19 +1,34 @@
 import * as React from "react";
+import { useState } from "react";
 import { State } from "./State/State";
 import "./States.less";
+import "../Country/Country.less";
+import "../More/More.less";
 
 export const States: React.FC = () => {
+  const [isActive, setActive] = useState(false);
+
+  const toggleClass = () => {
+    setActive(!isActive);
+  };
+
   return (
-    <div className="States">
-      <State
-        name={"Anglie"}
-        leagues={["Premier league", "Championship"]}
-        extendClass={"Anglie"}
-      />
-      <State name="Albanie" />
-      <State name="Egypt" />
-      <State name="Angola" />
-      <State name="Astralie" />
-    </div>
+    <>
+      <div className="Country">Země</div>
+      <div className="States">
+        <State
+          name={"Anglie"}
+          leagues={["Premier league", "Championship"]}
+          extendClass={"Anglie"}
+        />
+        <State name="Albanie" />
+        <State name="Egypt" />
+        <State name="Angola" />
+        <State name="Astralie" />
+      </div>
+      <div className={isActive ? "clicked" : "More"} onClick={toggleClass}>
+        Více
+      </div>
+    </>
   );
 };
