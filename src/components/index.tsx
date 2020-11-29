34d6b46b5b2c422fbd44d150bc2c884fe7b79a06
Ales Vachuska <aless.vachuska@seznam.cz>
2020-11-29 19:45:57 +0100

    zmeny

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import "./index.less";
+import "./variable.less";
 import { Container } from "./Container/Container";
 
 ReactDOM.render(<Container />, document.getElementById("root"));
